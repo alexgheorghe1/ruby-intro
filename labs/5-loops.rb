c5a873d8774d41a61b93e68b3fb460de8cc27444
alexgheorghe1 <Alex.gheorghe@kellogg.northwestern.edu>
2022-01-25 19:32:43 +0000

    class exercises pt 2

--- labs/5-loops.rb
+++ labs/5-loops.rb
@@ -8,3 +8,11 @@
 # screen.
 ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
 suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
+
+for values in ranks
+    for things in suits
+        puts "#{values} #{things}"
+    end
+end
+
+
